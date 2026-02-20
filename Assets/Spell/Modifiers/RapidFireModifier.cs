ccf9ea3e756824c83f66fa21b584831c7909d72f
Isaac Morrell <isaacmorrell13@gmail.com>
2026-02-19 22:03:18 -0800

    rapid fire modifier value fix

--- Assets/Spell/Modifiers/RapidFireModifier.cs
+++ Assets/Spell/Modifiers/RapidFireModifier.cs
@@ -11,9 +11,8 @@ public class RapidFireModifier : ModifierDefinition
     public override void CalculateStats(SpellDefinition definition)
     {
         definition.size *= 0.8f;
-        definition.speed *= 1.2f;
-        definition.cooldown *= 0.5f;
-        definition.multiplier *= 0.8f;
+        definition.speed *= 1.1f;
+        definition.cooldown *= 0.8f;
     }
     public override void OnCast(SpellInstance instance)
     {
